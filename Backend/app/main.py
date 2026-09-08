23687fe4dcc84f9dc5cce5c63c9a4bb833217040
Gabriel Borela <gabrielborela@MacBook-Air-de-Gabriel.local>
2026-09-08 15:15:25 -0300

    liberando cors

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -25,6 +25,7 @@ app.add_middleware(
         "https://xiaswap.netlify.app",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://xiaswap.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
